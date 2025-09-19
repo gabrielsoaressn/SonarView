61540f917a62407f31feeab687fd272c9f9ed4b7
Gabriel <gabrielsoaresjpb@gmail.com>
2025-09-19 02:53:41 -0300

    refactor: Remove unused JavaScript project files

--- src/App.js
+++
@@ -1,6 +0,0 @@
-// This file is included for completeness but the main app logic is in index.js
-// since we're using a single-file React setup with Babel standalone
-
-window.App = function App() {
-  return React.createElement('div', null, 'Aurora View App');
-};
